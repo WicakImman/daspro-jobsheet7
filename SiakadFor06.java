b8bbb0f00d3b36acf714808d0f6ed3636ed509a4
WicakImman <n18416665@gmail.com>
2025-10-23 12:31:52 +0700

    For modufy

--- SiakadFor06.java
+++ SiakadFor06.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class SiakadFor06 {
@@ -8,6 +7,8 @@ public class SiakadFor06 {
         double nilai, tertinggi, terendah;
         tertinggi = 0;
         terendah = 100;
+        int lulus = 0;
+        int tidakLulus = 0;
         
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -20,10 +21,19 @@ public class SiakadFor06 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            
+            // Cek kelulusan (batas minimal 60)
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
         
         System.out.println("\nNilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
         
         sc.close();
     }

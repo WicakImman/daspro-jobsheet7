a39e35ad7ab9fe8144f131a123be74d0af90f7c4
WicakImman <n18416665@gmail.com>
2025-10-23 17:37:52 +0700

    While

--- SiakadWhile06.java
+++ SiakadWhile06.java
@@ -4,6 +4,8 @@ public class SiakadWhile06 {
         Scanner sc = new Scanner(System.in);    
 
         int nilai, jml, i;
+        int lulus = 0, tidakLulus = 0;
+        int nilaiTertinggi = 0, nilaiTerendah = 100;
         i = 0;
 
         System.out.print("Masukkan jumlah mahasiswa: ");
@@ -14,6 +16,22 @@ public class SiakadWhile06 {
             nilai = sc.nextInt();
             
             if (nilai >= 0 && nilai <= 100) {
+                // Update nilai tertinggi dan terendah
+                if (nilai > nilaiTertinggi) {
+                    nilaiTertinggi = nilai;
+                }
+                if (nilai < nilaiTerendah) {
+                    nilaiTerendah = nilai;
+                }
+                
+                // Hitung mahasiswa lulus dan tidak lulus
+                if (nilai >= 60) {
+                    lulus++;
+                } else {
+                    tidakLulus++;
+                }
+                
+                // Tampilkan grade
                 if (nilai >= 80 && nilai <= 100) {
                     System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah A");
                     System.out.println("Bagus, pertahankan nilainya!");
@@ -35,6 +53,13 @@ public class SiakadWhile06 {
                 System.out.println("Nilai tidak valid! Masukkan nilai antara 0-100");
             }
         }
+        
+        // Tampilkan statistik
+        System.out.println("\n=== STATISTIK NILAI ===");
+        System.out.println("Nilai Tertinggi: " + nilaiTertinggi);
+        System.out.println("Nilai Terendah: " + nilaiTerendah);
+        System.out.println("Jumlah mahasiswa yang lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + tidakLulus);
         
         sc.close();
     }
